efb57c59b858ec9324b5294534ca14e93911c969
Chirag Vijayvergiya <111374446+chirag412vijayvergiya@users.noreply.github.com>
2024-11-09 16:37:46 +0530

    Day 3 (Functions, Modules)

---
+++ chirag.py
@@ -0,0 +1,6 @@
+n1=100
+def add(a, b):
+    print("The Addition is:" ,a+b)
+
+def sub(a,b):
+    print ("The Substraction:", a-b)
---
+++ driver.py
@@ -0,0 +1,20 @@
+# import  chirag 
+# chirag.add (10, 20)
+# chirag.sub (22,11)
+# print(chirag.n1)
+
+# import  chirag  as c
+# c.add (10, 20)
+# c.sub (22,11)
+# print(c.n1)
+
+# from chirag import add, sub, n1 
+# add (10, 20)
+# sub (22,11)
+# print(n1)
+
+from chirag import *
+add (10, 20)
+sub (22,11)
+print(n1)
+
